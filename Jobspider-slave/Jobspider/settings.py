8f99f6203efb42345a924c21e13e0e5fa4f22a99
AIshouldbecry <34427864+AIshouldbecry@users.noreply.github.com>
2018-12-05 22:21:35 +0800

    Add files via upload

--- Jobspider-slave/Jobspider/settings.py
+++ Jobspider-slave/Jobspider/settings.py
@@ -27,6 +27,12 @@ ITEM_PIPELINES = {
     'Jobspider.pipelines.JobspiderPipeline': 300,
     'scrapy_redis.pipelines.RedisPipeline': 301
 }
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+
+}
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
